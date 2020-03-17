7f1444b911c34c7b1d3858138e7132dd1a458509
TheekSkate <atheek.2018145@iit.ac.lk>
2020-03-17 15:04:45 +0530

    Adding 1. ExporterSignIn  2. FarmerSignIn

---
+++ Pehan/UNAGI/FarmerSignIn.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+
+import{ View, 
+        Text,
+        StyleSheet,
+        ImageBackground,
+        TextInput,
+        TouchableOpacity,
+       
+      } from 'react-native';
+      
+import vegBg from './images/vegBg.jpeg';
+
+
+
+
+class FarmerSignIn extends Component {
+
+  render() {
+  return (
+    <ImageBackground source={vegBg} 
+                     style= {styles.bgContainer}>
+
+   
+    <View>
+     
+    {/* HEADER */}
+    <View style = {styles.container}>
+        <Text style = {styles.headText}> UNAGI </Text>     
+    </View>
+   
+   
+   {/* All 4 inputs  */}
+   
+      <View>
+          <TextInput style= {styles.inputText} 
+          placeholder={'Email'}
+          placeholderTextColor={'#fff'} 
+          underlineColorAndroid={'#fff'} />
+      </View>
+      <View>
+          <TextInput style= {styles.inputText} 
+          placeholder={'Full name'}
+          placeholderTextColor={'#fff'} 
+          underlineColorAndroid={'#fff'} />
+      </View>
+      <View>
+          <TextInput style= {styles.inputText}
+ 
+          placeholder={'Password'}
+          secureTextEntry = {true}
+          placeholderTextColor={'#fff'} 
+          underlineColorAndroid={'#fff'} />
+      </View>
+      <View>
+          <TextInput style= {styles.inputText} 
+          placeholder={'Location'}
+          placeholderTextColor={'#fff'} 
+          underlineColorAndroid={'#fff'} />
+      </View>
+  
+      {/* SIGN IN BUTTON */}
+
+      <TouchableOpacity style={styles.btnSignIn}> 
+          <Text style={styles.btnText}> Sign in </Text>
+      </TouchableOpacity>
+
+    {/*  TEXT TO LOGIN */}
+
+      <Text  style={styles.loginText}> Have an account? Login </Text>
+
+    </View>
+    </ImageBackground> 
+  )
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  headText:{
+    paddingTop:100,
+    paddingBottom: 50,
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 10,
+    opacity: 0.5
+    
+  },
+
+  bgContainer:{
+    flex:1,
+    height: null,
+    width: null,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+
+  loginText: {
+      color:'#fff',
+      textAlign:"center",
+      marginTop: 120,
+      fontSize: 16 
+      
+  },
+
+
+  inputText:{
+    marginBottom: 20,
+    height:45,
+    fontSize: 16, 
+    paddingLeft: 35,
+    color: '#fff',
+    marginHorizontal: 25
+  },
+  btnSignIn:{
+      height:55,
+      borderRadius: 25,
+      backgroundColor: '#ffa477',
+      justifyContent: 'center',
+      width: 330,
+      marginLeft: 30,
+      marginTop: 40
+  },
+  btnText:{
+    color: '#000',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center'
+
+  },
+})
+
+export default FarmerSignIn ;
