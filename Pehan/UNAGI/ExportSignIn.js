17cd258e6e809449b3281d49df2d35b310430578
dhanush-sys <dhanushsivasamyg@gmail.com>
2020-04-01 20:24:37 +0530

    danny commiting

--- Pehan/UNAGI/ExportSignIn.js
+++ Pehan/UNAGI/ExportSignIn.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import{ View, 
         Text,
         StyleSheet,
@@ -16,6 +16,62 @@ import vegBg from './images/background.jpg';
 
 
 class ExporterSignIn extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+        Username:'',
+        Fullname:'',
+        Email:'',
+        Password:'',
+        Company:'',
+        success:''
+    };
+  }
+submit=() => {
+  if(Object.keys(this.state.Username).length==0){
+    alert("Please enter your Username")
+    this.setState({success:"notsuccess"})
+  }
+  if(Object.keys(this.state.Fullname).length==0){
+    alert("Please enter your Fullname")
+    this.setState({success:"notsuccess"})
+  }
+  if(Object.keys(this.state.Email).length==0){
+    alert("Please enter your Email")
+    this.setState({success:"notsuccess"})
+  }
+  if(Object.keys(this.state.Company).length==0){
+    alert("Please enter your Company")
+    this.setState({success:"notsuccess"})
+  }
+  if(Object.keys(this.state.Password).length==0){
+    alert("Please enter your Password")
+    this.setState({success:"notsuccess"})
+  }
+  if(Object.keys(this.state.success).length==0){
+    const URL="http://10.0.2.2:4000/exportersigninuserinfo"
+    const exporterconfirm = async () => {
+      try {
+        return await axios.post(URL,this.state)
+        } catch (error) {
+          console.error(error)
+        }
+      }
+      const getexporterconfirm = async () => {
+      const confirm = await exporterconfirm()
+    
+       if (confirm.data.message=="success") {
+          alert("Successfully added")
+         }
+      else if(confirm.data.message=="user"){
+        alert("This Username is already in used")
+      }
+      else
+      {alert("There is already a user with this email")}
+       }
+      getexporterconfirm();
+  }
+}
   render() {  
     return (
       <ImageBackground source={vegBg} style= {styles.bgContainer}>
@@ -33,6 +89,8 @@ class ExporterSignIn extends Component {
               <TextInput style= {styles.inputText} 
                 placeholder={'Email'}
                 placeholderTextColor={'#E59866'} 
+                onChangeText={text=>{this.setState({Email:text});this.setState({success:''})}}
+              />
                  />
             </View>
 
@@ -40,7 +98,10 @@ class ExporterSignIn extends Component {
                 <TextInput style= {styles.inputText} 
                 placeholder={'Full name'}
                 placeholderTextColor={'#E59866'} 
+                onChangeText={text=>{this.setState({Fullname:text});this.setState({success:''})}}
+                />
                  />
+
             </View>
 
             <View>
@@ -48,18 +109,31 @@ class ExporterSignIn extends Component {
                 placeholder={'Password'}
                 secureTextEntry = {true}
                 placeholderTextColor={'#E59866'} 
+                onChangeText={text=>{this.setState({Password:text});this.setState({success:''})}}
                  />
+            </View>
+            
+            <View>
+                <TextInput style= {styles.inputText} 
+                placeholder={'Username'}
+                placeholderTextColor={'#E59866'}
+                onChangeText={text=>{this.setState({Username:text});this.setState({success:''})}}
+                />
+                 />
+
             </View>
 
             <View>
                 <TextInput style= {styles.inputText} 
                 placeholder={'Company'}
                 placeholderTextColor={'#E59866'} 
+                onChangeText={text=>{this.setState({Company:text});this.setState({success:''})}}
+
                  />
             </View>
 
             {/* SIGN UP BUTTON */}
-            <TouchableOpacity style={styles.btnSignIn}> 
+            <TouchableOpacity style={styles.btnSignIn} onPress={this.submit}> 
                 <Text style={styles.btnText}> Sign Up </Text>
             </TouchableOpacity>
 
