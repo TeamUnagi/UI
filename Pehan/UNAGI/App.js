1d4f41396993f9ff2f2e837154e1502aab3595a5
dhanush-sys <dhanushsivasamyg@gmail.com>
2020-04-16 21:14:29 +0530

    danny commiting

--- Pehan/UNAGI/App.js
+++ Pehan/UNAGI/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 
 import ContractSendPage from './ContractSendPage.js';
-
-
+import UserPage from './UserPage.js'
+import FarmerProfDisplay from './FarmerProfDisplay'
 
 class App extends Component {
 
@@ -11,7 +11,7 @@ class App extends Component {
     return (
 
        
-       <ContractSendPage />
+       <FarmerProfDisplay />
     )
   }
 }
